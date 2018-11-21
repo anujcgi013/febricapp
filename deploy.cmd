0dec4b6e633d7aa4bb7d692fbdd0ade8740bc26c
Yadav Anujkumar (Consultant) <anujkumar.yadav@consultant.volvo.com>
2018-11-22 01:05:42 +0530

    target

--- deploy.cmd
+++ deploy.cmd
@@ -94,8 +94,8 @@ echo Handling node.js deployment.
 call :SelectNodeVersion
 
 :: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
+IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
+  pushd "%DEPLOYMENT_SOURCE%"
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
