7098b0f268677264ade691c65765559782edeece
Yadav Anujkumar (Consultant) <anujkumar.yadav@consultant.volvo.com>
2018-11-21 19:17:05 +0530

    first commit

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
 import { ProductsService } from '../services/products.service';
 import { product } from '../Model/Product';
-import { Guid } from 'guid-typescript';
 import { DatePipe} from '@angular/common';
 
 @Component({
@@ -10,12 +9,20 @@ import { DatePipe} from '@angular/common';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
+
 export class ProductComponent implements OnInit {
   productForm;
-  SerialId:string="1999";
+  bundleNumber:string;
   date=new Date();
+  ProductDetails:product;
+  show:boolean=true;
+  currentSerialNumber:number=1;
+  barcodeNumber:string;
+
+
   constructor(private formBuilder:FormBuilder, private pruductService:ProductsService,
     private datepipe:DatePipe) { 
+    
     this.productForm=this.formBuilder.group({
       bundleNumber:['', [Validators.required,isSymbols]],
       length:'',
@@ -26,34 +33,56 @@ export class ProductComponent implements OnInit {
   }
   
   ngOnInit() {
-  }
+    this.pruductService.getSerialNumber().subscribe(
+      (x:any) => {this.currentSerialNumber+=x;},
+      (y)=>{alert("Error in Fetching Serial Number")})
+    }
 
+barcodeControl:any;
+  saveProduct(){
+    var productdata:product={
+      companyId: 'E5E1377F-7275-4937-B9B4-4EF6BFD5EB8E',
+      bundleNumber: this.productForm.value.bundleNumber,
+      length: this.productForm.value.length,
+      weight:this.productForm.value.weight,
+      comments:this.productForm.value.comments,
+      createdDate:this.GetDate()
+    }
+    this.pruductService.postproducts(productdata).subscribe(
+      data=>{
+        alert('Your detail has been saved')
+        this.show=true
+        this.barcodeNumber = this.currentSerialNumber + ' ' + this.bundleNumber
+      },
+      (error)=>
+      alert('Not saved ---some Error')
+    )
+    }
 
- saveProduct(){
-  var productdata:product={
-    companyId: '2e3a0c4c-eb8d-4903-b729-38679ea404a6',
-    bundleNumber: this.productForm.value.bundleNumber,
-    length: this.productForm.value.length,
-    weight:this.productForm.value.weight,
-    comments:this.productForm.value.comments,
-    createdDate:this.GetDate()
+    GetDate():string
+    {
+      this.date=new Date();
+      let latest_date =this.datepipe.transform(this.date, 'yyyy-MM-dd');
+      return latest_date;
+    }
+
+    print(): void {
+      let printContents, popupWin;
+      printContents = document.getElementById('print-section').innerHTML;
+      popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+      popupWin.document.open();
+      popupWin.document.write(`
+        <html>
+          <head>
+            <style>
+            //........Customized style.......
+            </style>
+          </head>
+      <body onload="window.print();window.close()">${printContents}</body>
+        </html>`
+      );
+      popupWin.document.close();
   }
-  console.log(productdata);
-  this.pruductService.postproducts(productdata).subscribe(
-    data=>{
-      alert('Your review is saved')
-      this.ngOnInit();
-    },
-    (error)=>
-    alert('Not saved ---some Error')
-  )
-}
-GetDate():string
-{
-  this.date=new Date();
-  let latest_date =this.datepipe.transform(this.date, 'yyyy-MM-dd');
-  return latest_date;
-}
 }
 
 function isSymbols(input:FormControl){
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient,HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {product} from '../Model/Product';
 
 @Injectable()
 export class ProductsService {
@@ -15,12 +16,16 @@ export class ProductsService {
 
   postproducts(obj):Observable<object>{
     return this.http.post(
-      "http://febricclubapi.azurewebsites.net/API/product/productDetails",
+      "http://localhost:60244/api/product/productDetail",
     obj,
     this.httpOptions);
   }
 
-//   getProducts():Observable<Object>{
-//     return this.http.get("http://febricclubapp.azurewebsites.net/API/product");
-//  }
+  getProducts():Observable<Object>{
+    return this.http.get("http://localhost:60244/API/product");
+ }
+
+ getSerialNumber():Observable<object>{
+   return this.http.get<object>("http://localhost:60244/API/TopSerial");
+}
 }
